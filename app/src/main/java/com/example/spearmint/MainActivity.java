eacc8ed0d6d32868685ec5904b1c12a0cd9a0d2e
gavriellowie <gavriel@ualberta.ca>
2021-03-07 01:19:23 -0700

    created bottom navigation bar, but app crashes upon open, need to be fixed

--- app/src/main/java/com/example/spearmint/MainActivity.java
+++ app/src/main/java/com/example/spearmint/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.spearmint;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.my_nav);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.experimentFragment, R.id.searchFragment, R.id.notificationsFragment,
+                R.id.profileFragment)
+                .build();
+//
+        NavController navController = Navigation.findNavController(this, R.id.navHostfragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
+
 }
