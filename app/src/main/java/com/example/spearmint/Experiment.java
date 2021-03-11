d01b8c5f2fcf59196edf9333a92f44b3798d25ec
hvtraan <hvtran@ualberta.ca>
2021-03-11 15:59:32 -0700

    Updated Custom List

---
+++ app/src/main/java/com/example/spearmint/Experiment.java
@@ -0,0 +1,21 @@
+package com.example.spearmint;
+
+public class Experiment {
+    private String experimentDescription;
+    private String experimentRegion;
+
+    Experiment(String experimentDescription, String experimentRegion) {
+        this.experimentDescription = experimentDescription;
+        this.experimentRegion = experimentRegion;
+    }
+
+    public String getExperimentDescription() {
+        return this.experimentDescription;
+    }
+
+    public String getExperimentRegion() {
+        return this.experimentRegion;
+    }
+    
+
+}
