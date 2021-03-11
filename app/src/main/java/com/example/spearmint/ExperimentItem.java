78f99130718e7c0ca8270efe0011b3b73049cdbb
jiho Park <jiho1@ualberta.ca>
2021-03-11 15:58:06 -0700

    Added Search Function in Search Fragment.

---
+++ app/src/main/java/com/example/spearmint/ExperimentItem.java
@@ -0,0 +1,14 @@
+package com.example.spearmint;
+
+public class ExperimentItem {
+    private String aTitle;
+
+    public ExperimentItem(String title){
+        aTitle = title;
+    }
+    public String getaTitle(){
+        return aTitle;
+    }
+
+
+}
