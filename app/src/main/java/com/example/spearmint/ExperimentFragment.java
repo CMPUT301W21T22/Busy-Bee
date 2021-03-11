7ee9fa1b12901234f395655549f25a4940816b50
DoJeon1 <donghwan.dj@gmail.com>
2021-03-10 23:22:41 -0700

    Added tests to listview

--- app/src/main/java/com/example/spearmint/ExperimentFragment.java
+++ app/src/main/java/com/example/spearmint/ExperimentFragment.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -64,6 +66,14 @@ public class ExperimentFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_experiment, container, false);
+
+        String[] experiments = {"Experiment A", "Experiment B", "Experiment C"};
+
+        ListView listView = (ListView) view.findViewById(R.id.experiment_list);
+
+        ArrayAdapter<String> experimentAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, experiments);
+
+        listView.setAdapter(experimentAdapter);
 
     addExperiment = view.findViewById(R.id.button);
     addExperiment.setOnClickListener(new View.OnClickListener() {
