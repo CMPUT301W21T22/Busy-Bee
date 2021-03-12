f3d1dd21ac1685f25da323fa73d52002bdc38fd9
gavriellowie <gavriel@ualberta.ca>
2021-03-11 18:18:04 -0700

    Generated unique ID and created profile page

--- app/src/main/java/com/example/spearmint/MainActivity.java
+++ app/src/main/java/com/example/spearmint/MainActivity.java
@@ -8,22 +8,34 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        ProfileFragment userInfo = new ProfileFragment();
+//        userInfo.storeUniqueID();
         // Initialize Bottom Navigation View
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         // Initialize NavController
         NavController navController = Navigation.findNavController(this, R.id.navHostfragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        ProfileFragment userInfo = new ProfileFragment();
+        userInfo.storeUniqueID(this);
     }
 }
